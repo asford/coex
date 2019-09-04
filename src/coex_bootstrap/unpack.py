f99fe2d6e58ca3f0133d9b3c49445c4522b23bcd
Alex Ford <a.sewall.ford@gmail.com>
2019-09-04 01:18:06 -0700

    Backport which for py27 bootstrap, add bin paths.

--- src/coex_bootstrap/unpack.py
+++ src/coex_bootstrap/unpack.py
@@ -9,43 +9,29 @@ import zipfile
 logger = logging.getLogger(__name__)
 
 
-class ZipPkgs(object):
-    def __init__(self, target, fnmatch_pattern):
-        logger.debug("ZipPkgs target=%s fnmatch_pattern=%s")
-        self._zipfile = zipfile.ZipFile(target)
-        self.names = fnmatch.filter(self._zipfile.namelist(), fnmatch_pattern)
-
-    def open(self, name):
-        return self._zipfile.open(name)
-
-    def extract_cmd(self, coex_binaries, name):
-        return [coex_binaries.unzip, "-p", self._zipfile.filename, name]
-
-    def pkgs(self, coex_binaries):
-        return [PkgHandle(p, self.extract_cmd(coex_binaries, p)) for p in self.names]
+def zip_pkgs(target, fnmatch_pattern):
+    logger.debug("zip_pkgs target=%s fnmatch_pattern=%s")
+    _zipfile = zipfile.ZipFile(target)
+    return [
+        ZipPkgHandle(target, name)
+        for name in fnmatch.filter(_zipfile.namelist(), fnmatch_pattern)
+    ]
 
 
-class FilePkgs(object):
-    def __init__(self, target, glob_pattern):
-        _full_glob = os.path.join(target, glob_pattern)
-        logger.debug("FilePkgs target=%s glob_pattern=%s _full_glob=%s")
-        self._path = target
-        self.names = [os.path.relpath(p, target) for p in glob.glob(_full_glob)]
-
-    def extract_cmd(self, coex_binaries, name):
-        return ["cat", os.path.join(self._path, name)]
-
-    def pkgs(self, coex_binaries):
-        return [PkgHandle(p, self.extract_cmd(coex_binaries, p)) for p in self.names]
-
-
-class PkgHandle(object):
-    def __init__(self, name, extract_cmd):
+class ZipPkgHandle(object):
+    def __init__(self, target, name):
         # type: (str, str) -> None
+        self.target = target
         self.name = name
-        self.extract_cmd = extract_cmd
+
+    def __repr__(self):
+        return "ZipPkgHandle(target={self.target!r}, name={self.name!r})".format(
+            self=self
+        )
 
     def extract(self, coex_binaries, prefix_dir):
+
+        extract_cmd = [coex_binaries.unzip, "-p", self.target, self.name]
 
         untar_cmd = (
             [coex_binaries.tar]
@@ -58,16 +44,56 @@ class PkgHandle(object):
         )
 
         logging.debug(
-            "extract pkg=%s extract=%r untar=%r", self.name, self.extract_cmd, untar_cmd
+            "extract pkg=%s extract=%r untar=%r", self.name, extract_cmd, untar_cmd
         )
 
-        extract = subprocess.Popen(self.extract_cmd, stdout=subprocess.PIPE, bufsize=-1)
+        extract = subprocess.Popen(extract_cmd, stdout=subprocess.PIPE, bufsize=-1)
         untar = subprocess.Popen(untar_cmd, stdin=extract.stdout, bufsize=-1)
 
         extract.wait()
         if extract.returncode:
-            raise subprocess.CalledProcessError(extract.returncode, self.extract_cmd)
+            raise subprocess.CalledProcessError(extract.returncode, extract_cmd)
 
         untar.wait()
         if untar.returncode:
             raise subprocess.CalledProcessError(untar.returncode, untar_cmd)
+
+
+def file_pkgs(target, glob_pattern):
+    _full_glob = os.path.join(target, glob_pattern)
+    logger.debug("file_pkgs target=%s glob_pattern=%s _full_glob=%s")
+
+    names = [os.path.relpath(p, target) for p in glob.glob(_full_glob)]
+
+    return [FilePkgHandle(target, name) for name in names]
+
+
+class FilePkgHandle(object):
+    def __init__(self, target, name):
+        # type: (str, str) -> None
+        self.target = target
+        self.name = name
+
+    def __repr__(self):
+        return "FilePkgHandle(target={self.target!r}, name={self.name!r})".format(
+            self=self
+        )
+
+    def extract(self, coex_binaries, prefix_dir):
+        untar_cmd = (
+            [coex_binaries.tar]
+            # tar filtered through zstd
+            # Seeing errors on macos 10.13 image when using --use-compress-program
+            # with arguments, consider using a wrapper script if zstd arguments
+            # are needed.
+            + ["--use-compress-program", coex_binaries.zstd]
+            + ["-x", "-C", prefix_dir]
+        )
+
+        logging.debug("extract pkg=%s untar=%r", self.name, untar_cmd)
+
+        with open(os.path.join(self.target, self.name), "rb") as inf:
+            untar = subprocess.Popen(untar_cmd, stdin=inf, bufsize=-1)
+            untar.wait()
+            if untar.returncode:
+                raise subprocess.CalledProcessError(untar.returncode, untar_cmd)
